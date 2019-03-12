1aa9ed07b749906af0f4cab49b75be58a95c61eb
PIARDSylvian <piard.sylvian@gmail.com>
2019-03-12 08:55:57 +0100

    atjdr-11 : Register / reset password ajouter question secrète

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UserType extends AbstractType
 {
@@ -28,6 +29,14 @@ class UserType extends AbstractType
             ))
             ->add('pseudonym', TextType::class)
             ->add('email', EmailType::class)
+            ->add('secretQ', ChoiceType::class, [
+                'choices'  => [
+                    'rq_1' => 'rq_1',
+                    'rq_2' => 'rq_2',
+                    'rq_3' => 'rq_3',
+                ],
+            ])
+            ->add('secretR', TextType::class)
             ->add('password', RepeatedType::class, array(
                 'type' => PasswordType::class,
                 'first_options'  => array('label' => 'Password'),
