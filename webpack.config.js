c695b72768665158ad5ee17ff1291366a225e0c1
PIARD Sylvian <piard.sylvian@gmail.com>
2020-05-20 09:01:53 +0200

    atjdr-poney

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,10 @@ Encore
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
-    .copyFiles({from: './assets/images'})
+    .copyFiles({from: './assets/images',
+        to: 'images/[path][name].[ext]',
+        pattern: /\.(png|jpg|jpeg)$/
+    })
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
