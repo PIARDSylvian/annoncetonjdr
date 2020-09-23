857bc51c290ef8655518f10bddab5a3661482bdb
PIARD Sylvian <piard.sylvian@gmail.com>
2020-09-23 16:48:22 +0200

    config

--- public/index.php
+++ public/index.php
@@ -25,7 +25,6 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 
-echo 'app error ?';
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
